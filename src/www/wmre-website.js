8a8a45083c4724f03d99f14a25647e48faa4e615
GeorgeEverett <george.everett@wmca.org.uk>
2021-10-14 17:08:48 +0100

    fix search appearing on ds pages, add accoridion and disruption-indicator components

--- src/www/wmre-website.js
+++ src/www/wmre-website.js
@@ -5,6 +5,7 @@ import cookies from '../wmre/patterns/cookies/_example';
 import headerJs from '../wmre/patterns/header/_example';
 import searchFilterJs from '../wmre/patterns/search/search-filter/_example';
 import footerJs from '../wmre/patterns/footer/_example';
+import accordionsJS from '../wmre/components/accordion/_example';
 
 const icons = () => {
   // Ajax SVG in, SVGS are referenced in app (Icon component)
@@ -21,5 +22,5 @@ const icons = () => {
 
 window.addEventListener(
   'DOMContentLoaded',
-  (forEachPolyfill(), icons(), colorPalettes(), cookies(), headerJs(), searchFilterJs(), footerJs(), highlightJS)
+  (forEachPolyfill(), icons(), colorPalettes(), cookies(), headerJs(), searchFilterJs(), footerJs(), accordionsJS(), highlightJS)
 );
